d96a59261c4d9e37759bda6eff31c82e5ba8a2be
Jorge Muñoz Guerrero <131190211+JorgeMunoz1991@users.noreply.github.com>
2023-09-01 19:10:10 +0000

    navbar, css, logo y portada

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -13,56 +13,63 @@ export const Navbar = () => {
     };
 
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid text-center justify-content-center"> {/* Cambiado */}
-                <div class="container">
-                    <a class="navbar-brand" href="#">Home</a>
-                    <a class="navbar-brand" href="#">Protectors</a>
-                    <a class="navbar-brand" href="#">Match</a>
-                    <button class="btn btn-outline-success" id="openSearchButton" onClick={toggleSearch}>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid text-center justify-content-center">
+                <div className="container d-flex justify-content-between align-items-center">
+                    <div className="nav-links">
+                        <a className="navbar-brand" href="/">Home</a>
+                        <a className="navbar-brand" href="protectors">Protectors</a>
+                        <a className="navbar-brand" href="tips">Tips</a>
+                        <a className="navbar-brand" href="lostanimals">Lost animals</a>
+                        <a className="navbar-brand" href="voluntaryform">Voluntary form</a>
+                        <a className="navbar-brand" href="experiences">Experiences</a>
+                    </div>
+                    <button className="btn btn-outline-success" id="openSearchButton" onClick={toggleSearch}>
                         Open Search
                     </button>
                 </div>
             </div>
-            <div className={`container ${searchVisible ? "" : "d-none"}`} id="searchModalContent">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <form action="https://petshelter.miwuki.com/busqueda-avanzada-protectoras" method="POST">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="searchModalLabel">Advanced search</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeSearch}></button>
-                            </div>
-                            <div class="modal-body">
-                                <input type="hidden" name="_token" value="HmnY8nKjVRyXFWy9N95Gf0ftsuxgm8nz4NQKhTGS" />
-                                <div class="row">
-                                    <div class="col-md-5">
-                                        <label class="form-label" for="fl_pais">Country</label>
-                                        <select id="fl_pais" name="fl_pais" class="form-select">
-                                            {/* Country options here */}
-                                        </select>
-                                    </div>
-                                    <div class="col-md-7">
-                                        <label class="form-label" for="fl_provincia">City</label>
-                                        <select id="fl_provincia" name="fl_provincia" class="form-select" disabled>
-                                            <option selected value="">City</option>
-                                        </select>
+            {searchVisible && (
+                <div className="search-modal">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <form action="https://petshelter.miwuki.com/busqueda-avanzada-protectoras" method="POST">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="searchModalLabel">Advanced search</h5>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={closeSearch}></button>
+                                </div>
+                                <div className="modal-body">
+                                    <input type="hidden" name="_token" value="HmnY8nKjVRyXFWy9N95Gf0ftsuxgm8nz4NQKhTGS" />
+                                    <div className="row">
+                                        <div className="col-md-5">
+                                            <label className="form-label" htmlFor="fl_pais">Country</label>
+                                            <select id="fl_pais" name="fl_pais" className="form-select">
+                                                {/* Country options here */}
+                                            </select>
+                                        </div>
+                                        <div className="col-md-7">
+                                            <label className="form-label" htmlFor="fl_provincia">City</label>
+                                            <select id="fl_provincia" name="fl_provincia" className="form-select" disabled>
+                                                <option selected value="">City</option>
+                                            </select>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-light" data-bs-dismiss="modal" onClick={closeSearch}>Close</button>
-                                <button
-                                    type="submit"
-                                    class="btn btn-primary"
-                                    id="contactBtn"
-                                    onClick={() => gtag('event', 'pet_shelters_filter', {'app_name': 'MPS_WEB'})}>
-                                    Search
-                                </button>
-                            </div>
-                        </form>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-light" data-bs-dismiss="modal" onClick={closeSearch}>Close</button>
+                                    <button
+                                        type="submit"
+                                        className="btn btn-primary"
+                                        id="contactBtn"
+                                        onClick={() => gtag('event', 'pet_shelters_filter', {'app_name': 'MPS_WEB'})}>
+                                        Search
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </nav>
     );
 };
