9249feda24b4bb6914231801bf0a91a4f23187ec
Ayoze Gómez Rosa <130746651+ayozegr05@users.noreply.github.com>
2023-09-02 14:30:21 +0000

    Rating report and user endpoints Finished and Models FInished

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Volunteer, ExperiencesBlog
-from api.models import db, User, Rating 
-from api.models import db, User, Volunteer
+from api.models import db, User, Volunteer, ExperiencesBlog, Rating, Report
 from api.utils import generate_sitemap, APIException
 from sqlalchemy import func
 
@@ -91,51 +89,51 @@ def update_vote_stats(user_id):
         # Si no hay valoraciones, pon los valores en 0
         vote_count = 0
         sum_total_votes = 0
+        calculated_rating = 0
     else:
         total_rating_value = sum(rating.rating for rating in all_ratings)
         vote_count = total_ratings
         sum_total_votes = total_rating_value
+        calculated_rating = total_rating_value / total_ratings
     
-    # Actualiza los campos en la tabla "Rating"
-    Rating.query.filter_by(rated_id=user_id).update({
-        'vote_count': vote_count,
-        'sum_total_votes': sum_total_votes
-    })
-    
+    # Actualiza los campos en la tabla "User"
+    user = User.query.get(user_id)
+    user.vote_count = vote_count
+    user.sum_total_votes = sum_total_votes
+    user.calculated_rating = calculated_rating
     db.session.commit()
 
 
 @api.route('/ratings', methods=['POST'])
 def handle_ratings():
-    request_body = request.get_json()
-    
+    request_body = request.get_json() 
     rater_id = request_body.get('user_id')
     rated_id = request_body.get('rated_id')
     rating_value = request_body.get('rating')
-    
+
     if rater_id is None or rated_id is None or rating_value is None:
         return jsonify({"message": "Invalid request body"}), 400
-    
+
     # Verifica si los usuarios existen y son de los tipos adecuados
     rater = User.query.get_or_404(rater_id)
     rated = User.query.get_or_404(rated_id)
 
     # Verifica que los roles sean los adecuados
     allowed_roles = ['Person', 'AnimalShelter']
+
     if rater.role not in allowed_roles or rated.role not in allowed_roles:
         return jsonify({"message": "Invalid user roles"}), 400
 
     # Validar que el rater no esté votando a sí mismo
     if rater_id == rated_id:
         return jsonify({"message": "Users cannot rate themselves"}), 400
-    
+
     # Validar que el valor de la valoración esté entre 1 y 5
     if not (1 <= rating_value <= 5):
         return jsonify({"message": "Rating value must be between 1 and 5"}), 400
-    
+
     # Definir new_rating con un valor inicial None
     new_rating = None
-
     # Verificar si ya existe una calificación previa del mismo rater al mismo rated
     existing_rating = Rating.query.filter_by(rater_id=rater_id, rated_id=rated_id).first()
 
@@ -146,27 +144,21 @@ def handle_ratings():
         # Si no existe una calificación previa, agrega una nueva calificación
         new_rating = Rating(rater_id=rater_id, rated_id=rated_id, rating=rating_value)
         db.session.add(new_rating)
-    db.session.commit()
 
+    db.session.commit()
     # Obtén el usuario calificado y sus valoraciones
     rated_user = User.query.get(rated_id)
     all_ratings = Rating.query.filter_by(rated_id=rated_id).all()
-
     # Calcula el nuevo valor promedio de rating
     total_ratings = len(all_ratings)  # Obtenemos el número de votaciones directamente de la lista
     total_rating_value = sum(rating.rating for rating in all_ratings)
     new_rating_value = total_rating_value / total_ratings
-
     # Actualiza el campo "rating" en la tabla "User"
     rated_user.rating = new_rating_value
     db.session.commit()
-    
-   
-
     # Ahora, obtener la sumatoria de votos totales y el contador de votos para rated_id
     sum_total_votes = db.session.query(func.sum(Rating.rating)).filter_by(rated_id=rated_id).scalar()
     vote_count = db.session.query(func.count(Rating.rating)).filter_by(rated_id=rated_id).scalar()
-
     # Llamar a la función para calcular y actualizar los valores de sum_total_votes, vote_count y rating
     update_vote_stats(rated_id)
 
@@ -308,3 +300,78 @@ def handle_experience(id):
                        "experience_deleting": id,
                        }
       return response_body, 200
+   
+
+@api.route('/reports', methods=['POST', 'GET'])
+def handle_reports():
+    if request.method =='GET':
+        # response_body = {"message": "Esto devuelve el get del endpooint users"}
+        reports = db.session.execute(db.select(Report).order_by(Report.id)).scalars()
+        results =[item.serialize() for item in reports]
+        response_body ={
+            "message":"Esto devuelve el endpoint de userst el GET",
+            "results": results,
+            "status": "ok" }
+        return response_body, 200
+    if request.method == 'POST':
+        request_body = request.get_json()
+        user_id = request_body.get('user_id')  # Obtén el correo electrónico del usuario del cuerpo de la solicitud
+
+        # Busca el usuario por user_id en la base de datos
+        user = User.query.get(user_id)
+
+        if user is None:
+            return jsonify({"message": "Missing user in request body"}), 400
+
+        if user is None:
+            return jsonify({"message": "User not found"}), 404
+
+        report = Report(
+            title=request_body['title'],
+            body=request_body['body'],
+            user_id=user.id  # Usa el id del usuario
+        )
+
+        db.session.add(report)
+        db.session.commit()
+
+        response_body = {
+            "message": "Adding new report",
+            "status": "ok",
+            "new_report": request_body
+        }
+
+        return response_body, 200
+
+
+@api.route('/reports/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_report(id):
+  if request.method == 'GET': 
+    report = db.get_or_404(Report, id) # De la base de datos user estoy obteniendo el id definido en el argumento de la funcion.
+    print(report)
+    response_body = {
+        "status": "ok",
+        "results": report.serialize()
+    }
+    return response_body, 200
+  if request.method == 'PUT':
+    request_body = request.get_json()
+    report = db.get_or_404(Report, id)
+    report.body = request_body["body"]
+    report.title = request_body["title"]
+    db.session.commit() 
+    response_body = {"message": "Updated Report",
+                     "status": "ok",
+                     "user": request_body
+                     }
+    return response_body, 200
+  if request.method == 'DELETE':  
+    report = db.get_or_404(Report, id)
+    db.session.delete(report) 
+    db.session.commit()
+    response_body = {"message": "DELETED report",
+                     "status": "ok",
+                     "report_deleting": id,
+                     }
+    return response_body, 200
+
